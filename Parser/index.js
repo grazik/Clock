835f5d10639d59e2187415b1695a87aacc1dc03e
grazik <grazik@grazik>
2018-05-27 22:06:41 +0200

    bugfix/parser deletes double spaces

--- Parser/index.js
+++ Parser/index.js
@@ -7,29 +7,29 @@ function convert() {
     const code = document.getElementById('code').value.split('\n');
 
     const vertices = code.filter(line => line.match(/^v /g))
-        .map(line => line.split(' '))
+        .map(line => line.replace(/\s+/g,' ').split(' '))
         .map(elem => `${elem[1]}, ${elem[2]}, ${elem[3]}`);
 
     const vertex_texture = code.filter(line => line.match(/^vt /g))
-        .map(line => line.split(' '))
+        .map(line => line.replace(/\s+/g,' ').split(' '))
         .map(elem => `${elem[1]}, ${elem[2]}`);
 
     const vertex_normals = code.filter(line => line.match(/^vn /g))
-        .map(line => line.split(' '))
+        .map(line => line.replace(/\s+/g,' ').split(' '))
         .map(elem => `${elem[1]}, ${elem[2]}, ${elem[3]}`);
     
     const faces = code.filter(line => line.match(/^f /g))
-        .map(line => line.split(' '))
+        .map(line => line.replace(/\s+/g,' ').split(' '))
         .map(line => [line[1],line[2],line[3]])
         .map(line => line.map(elem => elem.split('/')))
         .map(line => line.map(coords => 
             [vertices[coords[0]-1]+', 1.0f', vertex_texture[coords[1]-1], vertex_normals[coords[2]-1]+', 0.0f']
         ));
 
-    console.log(faces);
+    //console.log(faces);
 
     const out = document.getElementById('output');
-    console.log(out);
+    //console.log(out);
     let h1, p, btn_copy;
 
     const num_of_triangles = faces.length*3;
@@ -40,8 +40,8 @@ function convert() {
     p.innerHTML = num_of_triangles;
     out.appendChild(p);
 
-    console.log('num_of_triangles');
-    console.log(num_of_triangles);
+    //console.log('num_of_triangles');
+    //console.log(num_of_triangles);
 
     //---------------------------
 
@@ -61,8 +61,8 @@ function convert() {
     p.innerHTML = vertex_array;
     out.appendChild(p);
     
-    console.log('vertex');
-    console.log(vertex_array);
+    //console.log('vertex');
+    //console.log(vertex_array);
 
     //---------------------------
 
@@ -82,8 +82,8 @@ function convert() {
     p.innerHTML = vertex_texture_array;
     out.appendChild(p);
     
-    console.log('vertex_texture');
-    console.log(vertex_texture_array);
+    //console.log('vertex_texture');
+    //console.log(vertex_texture_array);
 
     //---------------------------
 
@@ -103,8 +103,8 @@ function convert() {
     p.innerHTML = vertex_normals_array;
     out.appendChild(p);
     
-    console.log('vertex_normals');
-    console.log(vertex_normals_array);
+    //console.log('vertex_normals');
+    //console.log(vertex_normals_array);
 
 }
 
