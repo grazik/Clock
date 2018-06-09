5e4e9b23a4de189c670a2adf390cf00848243d33
grazik <grazik@grazik>
2018-06-09 09:54:59 +0200

    feature/animate-gear changed Model class structure

--- Zegar/Models/AllModels.h
+++ Zegar/Models/AllModels.h
@@ -1,4 +1,3 @@
 #pragma once
 
-#include "Models/TeaPot/teapot.h"
 #include "Models/Gear/Gear.h"
--- Zegar/Models/Model.h
+++ Zegar/Models/Model.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <vector>
-#include <glm/glm.hpp>
-#include <GLFW/glfw3.h>
+#include "config.h"
+#include "Classes/headers/Shader.h"
+#include "Classes/headers/Graphics.h"
 
 class Model {
 	public:
@@ -12,17 +13,26 @@ class Model {
 		float* getVertexNormals() { return vertexNormals; }
 		float* getTexCoords() { return texCoords; }
 		std::string getTexName() { return texName; }
+		std::string getName() { return name; }
+		Shader* getShader() { return shader; }
 
 		void setTextName(std::string name) { texName = name; }
+		void setName(std::string newName) { name = newName; }
 		void setVertexCount(int vertexNumber) { vertexCount = vertexNumber; }
 		void setVertices(float* verticesArray) { vertices = verticesArray; }
 		void setVertexNormals(float* vertexNormalsArray) { vertexNormals = vertexNormalsArray; }
 		void setTexCoords(float* texCoordsArray) { texCoords = texCoordsArray; }
+		void setShader(Shader* newShader) { shader = newShader; }
+
+		virtual void drawObject() = 0;
+		virtual void prepareObject() = 0;
 
 	private:
 		std::string texName;
+		std::string name;
 		int vertexCount;
 		float* vertices;
 		float* vertexNormals;
 		float* texCoords;
+		Shader* shader;
 };
