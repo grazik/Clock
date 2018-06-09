fab998714d41208fba79371243f3017cd32bfe5c
grazik <grazik@grazik>
2018-06-09 10:00:36 +0200

    feature/animate-gear changed config structure

--- Zegar/config.h
+++ Zegar/config.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <string>
+#include "Classes/headers/Shader.h"
+
 namespace config {
-	float cameraX = -5.0f;
-	float cameraY = 0.0f;
-	float cameraZ = 0.0f;
-	float cameraRotationAngleChange = 5.0f;
-	float cameraMoveAngleChange = 10.0f;
-	float cameraMoveFactor = 0.15f;
+	extern float cameraX;
+	extern float cameraY;
+	extern float cameraZ;
+	extern float cameraRotationAngleChange;
+	extern float cameraMoveAngleChange;
+	extern float cameraMoveFactor;
+	extern std::string modelPath;
 }
